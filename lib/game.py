dadb92b6794179bf6736af9754b21c5dc06484ed
David Sveningsson <ext@sidvind.com>
2011-07-21 15:47:20 +0200

    moving music

--- lib/game.py
+++ lib/game.py
@@ -93,7 +93,7 @@ class Game(object):
 
         # Play background "music" if audio was initialized properly.
         if pygame.mixer.get_init() is not None:
-            self.music = pygame.mixer.Sound('nyan.wav')
+            self.music = pygame.mixer.Sound('sound/nyan.wav')
             self.music.play(loops=-1)
 
         self.goal = Sprite('goal.png', 1)
